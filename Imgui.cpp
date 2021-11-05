1be3fe71a5fbcc7d3210e71557a232067dcb3bf7
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2021-11-05 20:50:40 +0000

    working tile save and load in json

--- Imgui.cpp
+++ Imgui.cpp
@@ -6,33 +6,71 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 #include <vector>
-int main() {
 
-    std::vector<int> tiles = {
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
-        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
-        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
-        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
-        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
-        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+#include "nlohmann/json.hpp"
+#include <fstream>
+
+using namespace std;
+using json = nlohmann::json;
+
+namespace Maps
+{
+    struct Map
+    {
+        unsigned int tileDimensions_x;
+        unsigned int tileDimensions_y;
+        vector<int> tileSet;
+        int width;
+        int height;
     };
-    std::string tileset = "tileset.png";
-    sf::Vector2u tileDimensions = sf::Vector2u(32, 32);
-    int width = 16;
-    int height = 8;
 
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "ImGui + SFML = <3");
+    json Save(Map& map)
+    {
+        json jsonMap;
+
+        jsonMap["tile_dimensions_x"] = map.tileDimensions_x;
+        jsonMap["tile_dimensions_y"] = map.tileDimensions_y;
+        jsonMap["tileSet"] = map.tileSet;
+        jsonMap["width"] = map.width;
+        jsonMap["height"] = map.height;
+
+        return jsonMap;
+    }
+
+    Map Load(json& jsonMap)
+    {
+        Map map;
+        jsonMap.at("tile_dimensions_x").get_to(map.tileDimensions_x);
+        jsonMap.at("tile_dimensions_y").get_to(map.tileDimensions_y);
+        jsonMap.at("tileSet").get_to(map.tileSet);
+        jsonMap.at("width").get_to(map.width);
+        jsonMap.at("height").get_to(map.height);
+
+        return map;
+    }
+};
+
+int main() {
+    ifstream inJson;
+
+    inJson.open("MapLevel.json"); //open file make stream
+
+    json jsonMap;
+
+    inJson >> jsonMap;
+
+    Maps::Map levelMap = Maps::Load(jsonMap);
+
+    sf::RenderWindow window(sf::VideoMode(levelMap.tileDimensions_x * levelMap.width, levelMap.tileDimensions_y * levelMap.height), "ImGui + SFML = <3"); //Make renderview size of tileset
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    TileMap tileMap;
+    TileMap tileMap(sf::Vector2u(levelMap.tileDimensions_x, levelMap.tileDimensions_y), levelMap.tileSet, levelMap.width, levelMap.height);
 
-    tileMap.load(tileset, tileDimensions, tiles, width, height);
+    tileMap.load("tileset.png");
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -43,6 +81,15 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+        }
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            auto mousePosition = sf::Mouse::getPosition(window);
+
+            cout << mousePosition.x / levelMap.tileDimensions_x << " , " << mousePosition.y / levelMap.tileDimensions_y << endl;
+
+            tileMap.changeTileValue(sf::Vector2i(mousePosition.x / levelMap.tileDimensions_x, mousePosition.y / levelMap.tileDimensions_y), 2);
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
@@ -59,6 +106,16 @@ int main() {
         ImGui::SFML::Render(window);
         window.display();
     }
+    //Save
+    ofstream outJson;
+
+    outJson.open("MapLevel.json"); //open file make stream
+
+    json outJsonMap = Maps::Save(levelMap);
+
+    outJson << outJsonMap;
+
+    outJson.close();
 
     ImGui::SFML::Shutdown();
 }
---
+++ Tilemap.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    TileMap(sf::Vector2u tileSize, std::vector<int>& tiles, unsigned int width, unsigned int height)
+    {
+        m_tileSize = tileSize;
+        m_tiles = &tiles;
+        m_width = width;
+        m_height = height;
+    }
+
+    bool load(const std::string& tileset)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(m_width * m_height * 4);
+
+        reDrawTexture();
+
+        return true;
+
+    }
+
+    void reDrawTexture()
+    {
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < m_width; ++i) {
+            for (unsigned int j = 0; j < m_height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = m_tiles->at(i + j * m_width);
+
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / m_tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / m_tileSize.x);
+
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * m_width) * 4];
+
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * m_tileSize.x, j * m_tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * m_tileSize.x, j * m_tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * m_tileSize.x, (j + 1) * m_tileSize.y);
+                quad[3].position = sf::Vector2f(i * m_tileSize.x, (j + 1) * m_tileSize.y);
+
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * m_tileSize.x, tv * m_tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * m_tileSize.x, tv * m_tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * m_tileSize.x, (tv + 1) * m_tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * m_tileSize.x, (tv + 1) * m_tileSize.y);
+            }
+        }
+    }
+
+    void changeTileValue(sf::Vector2i position, int changedVal)
+    {
+        if(position.x >= 0 && position.x < m_width && position.y >= 0 && position.y < m_height)
+        {
+            m_tiles->at(position.x + (position.y * m_width)) = changedVal;
+
+            reDrawTexture();
+        }
+    }
+
+private:
+
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+    sf::Vector2u m_tileSize;
+	std::vector<int>* m_tiles;
+    unsigned int m_width;
+    unsigned int m_height;
+
+};
